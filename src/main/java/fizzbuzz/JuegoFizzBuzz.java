44ea63c70941b009923c6d6edd7e13a4917cfed8
RosaPs <rosapacheco.contact@gmail.com>
2021-11-30 10:55:17 +0100

    Test comprobados y terminados

--- src/main/java/fizzbuzz/JuegoFizzBuzz.java
+++ src/main/java/fizzbuzz/JuegoFizzBuzz.java
@@ -3,6 +3,28 @@ package fizzbuzz;
 public class JuegoFizzBuzz {
 
     public String responder(int numero) {
-        return String.valueOf(numero);
+        String respuesta1 = "Fizz";
+        String respuesta2 = "Buzz";
+        //String respuesta3 = "FizzBuzz";
+        if(numero >=1 && numero <=100){
+            if (numero % 3 ==0 && numero % 5 == 0){
+
+                return respuesta1 + respuesta2;
+            }else if (numero % 5 == 0){
+
+                return respuesta2;
+            }else if (numero % 3 == 0){
+
+                return respuesta1;
+            }else {
+                return String.valueOf(numero);
+            }
+        }else{
+            return "El número es mayor del permitido";
+        }
+
+
     }
+
+
 }
--- src/test/java/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -24,4 +24,38 @@ public class FizzBuzzTest {
         String respuesta = juego.responder(2);
         assertThat(respuesta, equalTo("2"));
     }
+
+    @Test
+    void laRespuestaA3EsFizz(){
+        String respuesta = juego.responder(3);
+        assertThat(respuesta, equalTo("Fizz"));
+    }
+    @Test
+    void laRespuestaA4Es4(){
+        String respuesta = juego.responder(4);
+        assertThat(respuesta, equalTo("4"));
+    }
+
+    @Test
+    void laRepuestaA5EsBuzz(){
+        String respuesta = juego.responder(5);
+        assertThat(respuesta, equalTo("Buzz"));
+    }
+
+    @Test
+    void laREspuestaA5yA3esFizzBuzz(){
+        String respuesta = juego.responder(15);
+        assertThat(respuesta, equalTo("FizzBuzz"));
+    }
+    @Test
+    void laRespuestaA66EsFizz(){
+        String respuesta = juego.responder(66);
+        assertThat(respuesta, equalTo("Fizz"));
+    }
+
+    @Test
+    void laRespuestaEstaEntre1y100(){
+        String respuesta = juego.responder(101);
+        assertThat(respuesta, equalTo("El número es mayor del permitido"));
+    }
 }
